145f7afe207513df9e3bcedf47c4f250878021cc
Hayden Davis <1hayden.davis@gmail.com>
2020-09-14 19:43:13 -0400

    pre-router work, added auth

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,12 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://ntsf3hvkovgqldgpcnq6bhlrpe.appsync-api.us-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-xvnpsut7czbevazakomnicv4bm"
+    "aws_appsync_apiKey": "da2-xvnpsut7czbevazakomnicv4bm",
+    "aws_cognito_identity_pool_id": "us-west-2:a8d8f984-0768-4b71-a670-c11c90336b88",
+    "aws_cognito_region": "us-west-2",
+    "aws_user_pools_id": "us-west-2_iD5Fh9tUX",
+    "aws_user_pools_web_client_id": "123k1399m2mebpl05p0vbce9j9",
+    "oauth": {}
 };
 
 
